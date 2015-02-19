8bd89f5d1998e657b3083f85b84dae39f0fda5c3
Abbas Dharamsey <adharams@ucsd.edu>
2015-02-19 13:36:11 -0800

    finished the lab

--- routes/project.js
+++ routes/project.js
@@ -4,7 +4,11 @@ exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
   // query for the specific project and
-  // call the following callback
+  // call the follow
+  //Cing callback
+  models.Project
+    .find({"_id": projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -15,13 +19,23 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newProject = new models.Project(form_data);
+  newProject.save(afterSaving);
 
+  function afterSaving(err) {
+    res.send('OK');
+  }
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(res.send());
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
